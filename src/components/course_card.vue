71a5cb9b25cd61f3dbc9b1493a52cef13123304e
64070040Thanakorn <88426940+64070040Thanakorn@users.noreply.github.com>
2023-03-01 21:40:57 +0700

    Update course_card.vue

--- src/components/course_card.vue
+++ src/components/course_card.vue
@@ -7,14 +7,27 @@ export default {
 <template>
     <section>
         <router-link to="/course">
-            <div class="bg-gray-2 w-[270px] h-[400px] p-5 mr-4">
+            <div class="bg-gray-2 w-[270px] p-5 mr-4 rounded-[10px]">
+                <!-- image -->
                 <div>
                     <img src="../assets/landing_image.png" alt="">
                 </div>
+                <!-- category -->
                 <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores voluptatum consequatur voluptate
-                        vero quod earum at, ex porro distinctio aspernatur error sunt rerum pariatur modi incidunt fuga sit
-                        deserunt numquam!</p>
+                    <p class="text-yellow-1">UX/UI design</p>
+                </div>
+                <!-- name -->
+                <div>
+                    <p>Usability Design and Psychology for Digital Products</p>
+                </div>
+                <!-- description -->
+                <div>
+                    <p class="text-gray-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores voluptatum consequatur voluptate
+                        vero quod earum at</p>
+                </div>
+                <!-- price -->
+                <div class="flex justify-end">
+                    <p>1990.0-</p>
                 </div>
             </div>
         </router-link>
