203b6538d7834a9d5542f0fac1bb73d066ff6b6b
64070040Thanakorn <88426940+64070040Thanakorn@users.noreply.github.com>
2023-03-02 20:50:44 +0700

    Update profile_selecter.vue

--- src/components/profile_selecter.vue
+++ src/components/profile_selecter.vue
@@ -20,25 +20,25 @@ export default {
     <div class="flex basis-3/12">
         <div class="flex flex-col">
             <div>
-                <router-link to="profile_course" class="text-gray-1">
+                <router-link :to="{ path: '/profile/' + '101' + '/profile_course' }" class="text-gray-1">
                     <i class="fa-solid fa-pen"></i>
                     <span class="ml-2">{{course}}</span>
                 </router-link>
             </div>
             <div>
-                <router-link to="profile_edit" class="text-gray-1">
+                <router-link :to="{ path: '/profile/' + '101' + '/profile_edit' }" class="text-gray-1">
                     <i class="fa-solid fa-user-pen"></i>
                     <span class="ml-2">{{edit}}</span>
                 </router-link>
             </div>
             <div>
-                <router-link to="profile_history" class="text-gray-1">
+                <router-link :to="{ path: '/profile/' + '101' + '/profile_history' }" class="text-gray-1">
                     <i class="fa-sharp fa-solid fa-clock-rotate-left"></i>
                     <span class="ml-2">{{history}}</span>
                 </router-link>
             </div>
             <div>
-                <router-link to="profile_payment" class="text-gray-1">
+                <router-link :to="{ path: '/profile/' + '101' + '/profile_payment' }" class="text-gray-1">
                     <i class="fa-regular fa-credit-card"></i>
                     <span class="ml-2">{{payment}}</span>
                 </router-link>
